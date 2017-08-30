9d422754424e5a208a0b6641484f7f4ceac2bb4d
Vyrrhus <baptistedesoubrie@gmail.com>
2017-08-31 01:54:44 +0200

    reupload form.js from desktop repository

--- javascript/form.js
+++ javascript/form.js
@@ -1,5 +1,6 @@
-var departForm = document.getElementById('form_depart');
+// Déclaration des variables
 
+var departForm = document.getElementById('form_depart');
 var inc = departForm.elements['inclinaison'];
 var W = departForm.elements['longitude'];
 var hPeri = departForm.elements['perigee'];
@@ -11,43 +12,9 @@ var v = departForm.elements['anomaly'];
 var rayon = 6371.008;
 var altitude_unit = document.getElementsByName('altitude_unit');
 
-inc.addEventListener('keyup', function(){
-    var inclinaison = parseInt(inc.value, 10);
-    var bool = isNaN(inclinaison);
-    if(inclinaison == 0 || bool) {
-        W.setAttribute('disabled','true');
-        W.value = '';
-    }
-    else {
-        W.removeAttribute('disabled');
-    }
-});
 
-hPeri.addEventListener('keyup', function parametre() {
-    var Apo = parseInt(hApo.value, 10) + rayon;
-    var Peri = parseInt(hPeri.value, 10) + rayon;
-    if (Peri <= Apo) {
-    departForm.elements['ecc'].value = (Apo - Peri) / (Apo + Peri) ;
-    departForm.elements['demiaxe'].value = (Apo + Peri) / 2 ;
-    }
-    else {
-        hApo.value=hPeri.value;
-        parametre()
-    }
-});
 
-hApo.addEventListener('keyup', function parametre() {
-    var Apo = parseInt(hApo.value, 10) + rayon;
-    var Peri = parseInt(hPeri.value, 10) + rayon;
-    if (Peri <= Apo) {
-    departForm.elements['ecc'].value = (Apo - Peri) / (Apo + Peri) ;
-    departForm.elements['demiaxe'].value = (Apo + Peri) / 2 ;
-    }
-    else {
-        hPeri.value=hApo.value;
-        parametre()
-    }
-});
+// Déclaration des fonctions
 
 function km_vers_UA(valeur) {
     var UA = 149597870.7;
@@ -60,6 +27,223 @@ function UA_vers_km(valeur) {
     valeur *= UA;
     return valeur;
 }
+
+function warning(elementDeReference, bool) {
+    var parent = elementDeReference.parentNode;
+    parent = parent.parentNode;
+    var contenu = parent.getAttribute('class');
+    var help = parent.lastElementChild;
+    if (bool) {
+        contenu += ' has-error';
+        parent.setAttribute('class', contenu);
+        help.removeAttribute('hidden');
+    }
+    else {
+        parent.setAttribute('class', 'form-inline form-group');
+        help.setAttribute('hidden','true');
+    }
+}
+
+
+
+
+// Evenements
+
+inc.addEventListener('keyup', function() {
+    var valeur = inc.value;
+    var condition = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.','-'];
+    var longueur = valeur.length;
+    var error=0;
+    var virgule=0;
+    for (var i=0 ; i < longueur ; i++) {
+        var caractere = valeur[i];
+        if (!condition.includes(caractere)) {
+            error++;
+        }
+        if (valeur[i] == '.') {
+            virgule++;
+        }
+        if (valeur[i] == '-' && i>0) {
+            error++;
+        }
+    }
+    valeur = parseFloat(valeur);
+    if (error > 0 || virgule > 1 || valeur > 180 || valeur < -180) {
+        warning(inc, true);
+    }
+    else {
+        warning(inc, false);
+    }
+    if(valeur == 0 || isNaN(valeur)) {
+        W.setAttribute('disabled','true');
+        W.value='';
+    }
+    else {
+        W.removeAttribute('disabled');
+    }
+});
+
+
+
+W.addEventListener('keyup', function() {
+    var valeur = W.value;
+    var condition = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+    var longueur = valeur.length;
+    var error=0;
+    var virgule=0;
+    for (var i=0 ; i < longueur ; i++) {
+        var caractere = valeur[i];
+        if (!condition.includes(caractere)) {
+            error++;
+        }
+        if (valeur[i] == '.') {
+            virgule++;
+        }
+    }
+    valeur = parseFloat(valeur);
+    if (error > 0 || virgule > 1 || valeur < 0 || valeur > 360) {
+        warning(W, true);
+    }
+    else {
+        warning(W, false);
+    }
+});
+
+hPeri.addEventListener('keyup', function parametre() {
+    var valeur = hPeri.value;
+    var condition = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+    var longueur = valeur.length;
+    var error=0;
+    var virgule=0;
+    for (var i=0 ; i < longueur ; i++) {
+        var caractere = valeur[i];
+        if (!condition.includes(caractere)) {
+            error++;
+        }
+        if (valeur[i] == '.') {
+            virgule++;
+        }
+    }
+    valeur = parseFloat(valeur);
+    if (error > 0 || virgule > 1) {
+        warning(hPeri, true);
+    }
+    else {
+        warning(hPeri, false);
+        var Apo = parseFloat(hApo.value, 10);
+        if (valeur <= Apo) {
+            e.value = (Apo - valeur) / (Apo + valeur + 2 * rayon);
+            a.value = (Apo + valeur) / 2 + rayon;
+        }
+        else {
+            hApo.value = valeur;
+            parametre();
+        }
+    }
+});
+
+
+
+
+hApo.addEventListener('keyup', function() {
+    var valeur = hApo.value;
+    var condition = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+    var longueur = valeur.length;
+    var error=0;
+    var virgule=0;
+    for (var i=0 ; i < longueur ; i++) {
+        var caractere = valeur[i];
+        if (!condition.includes(caractere)) {
+            error++;
+        }
+        if (valeur[i] == '.') {
+            virgule++;
+        }
+    }
+    valeur = parseFloat(valeur);
+    if (error > 0 || virgule > 1) {
+        warning(hApo, true);
+    }
+    else {
+        warning(hApo, false);
+    }
+});
+
+
+
+hApo.addEventListener('keypress', function parametre() {
+    var Apo = parseInt(hApo.value, 10) + rayon;
+    var Peri = parseInt(hPeri.value, 10) + rayon;
+    if (Peri <= Apo) {
+    departForm.elements['ecc'].value = (Apo - Peri) / (Apo + Peri) ;
+    departForm.elements['demiaxe'].value = (Apo + Peri) / 2 ;
+    }
+    else {
+        hPeri.value=hApo.value;
+        parametre()
+    }
+});
+
+
+
+
+
+
+
+
+
+
+w.addEventListener('keyup', function() {
+    var valeur = w.value;
+    var condition = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+    var longueur = valeur.length;
+    var error=0;
+    var virgule=0;
+    for (var i=0 ; i < longueur ; i++) {
+        var caractere = valeur[i];
+        if (!condition.includes(caractere)) {
+            error++;
+        }
+        if (valeur[i] == '.') {
+            virgule++;
+        }
+    }
+    valeur = parseFloat(valeur);
+    if (error > 0 || virgule > 1 || valeur > 360) {
+        warning(w, true);
+    }
+    else {
+        warning(w, false);
+    }
+});
+
+v.addEventListener('keyup', function() {
+    var valeur = v.value;
+    var condition = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+    var longueur = valeur.length;
+    var error=0;
+    var virgule=0;
+    for (var i=0 ; i < longueur ; i++) {
+        var caractere = valeur[i];
+        if (!condition.includes(caractere)) {
+            error++;
+        }
+        if (valeur[i] == '.') {
+            virgule++;
+        }
+    }
+    valeur = parseFloat(valeur);
+    if (error > 0 || virgule > 1) {
+        warning(v, true);
+    }
+    else {
+        warning(v, false);
+    }
+});
+
+
+
+
 
 for (var i=0, c=altitude_unit.length ; i < c ; i++){
     altitude_unit[i].addEventListener('click', function(){
