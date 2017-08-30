25cab41abb32cc57982b618ee10b5502514fcc26
Vyrrhus <baptistedesoubrie@gmail.com>
2017-08-30 20:05:45 +0200

    reupload form.js from desktop repository

--- javascript/form.js
+++ javascript/form.js
@@ -1,47 +1,89 @@
 var departForm = document.getElementById('form_depart');
 
-var i = departForm.elements['inclinaison'];
+var inc = departForm.elements['inclinaison'];
 var W = departForm.elements['longitude'];
 var hPeri = departForm.elements['perigee'];
 var hApo = departForm.elements['apogee'];
 var e = departForm.elements['ecc'];
-var p = departForm.elements['parameter'];
+var a = departForm.elements['demiaxe'];
 var w = departForm.elements['argument'];
 var v = departForm.elements['anomaly'];
 var rayon = 6371.008;
+var altitude_unit = document.getElementsByName('altitude_unit');
 
+inc.addEventListener('keyup', function(){
+    var inclinaison = parseInt(inc.value, 10);
+    var bool = isNaN(inclinaison);
+    if(inclinaison == 0 || bool) {
+        W.setAttribute('disabled','true');
+        W.value = '';
+    }
+    else {
+        W.removeAttribute('disabled');
+    }
+});
 
-hPeri.addEventListener('blur', function parametre() {
+hPeri.addEventListener('keyup', function parametre() {
     var Apo = parseInt(hApo.value, 10) + rayon;
     var Peri = parseInt(hPeri.value, 10) + rayon;
     if (Peri <= Apo) {
     departForm.elements['ecc'].value = (Apo - Peri) / (Apo + Peri) ;
-    departForm.elements['parameter'].value = (2 * Apo * Peri)/(Apo + Peri) ;
+    departForm.elements['demiaxe'].value = (Apo + Peri) / 2 ;
     }
     else {
-        alert("Tu crois sérieusement que le périgée peut être plus grand que l'apogée ?");
         hApo.value=hPeri.value;
         parametre()
     }
-    var a = (Apo + Peri)/2;
-    var periode = 2 * Math.PI * Math.sqrt(a*a*a/398600.4418);
-    document.getElementById('periode').innerHTML = periode + "s";
 });
 
-hApo.addEventListener('blur', function parametre() {
+hApo.addEventListener('keyup', function parametre() {
     var Apo = parseInt(hApo.value, 10) + rayon;
     var Peri = parseInt(hPeri.value, 10) + rayon;
     if (Peri <= Apo) {
     departForm.elements['ecc'].value = (Apo - Peri) / (Apo + Peri) ;
-    departForm.elements['parameter'].value = (2 * Apo * Peri)/(Apo + Peri) ;
+    departForm.elements['demiaxe'].value = (Apo + Peri) / 2 ;
     }
     else {
-        alert("Tu crois sérieusement que l'apogée peut être plus petit que le périgée ?");
         hPeri.value=hApo.value;
         parametre()
     }
-    var a = (Apo + Peri)/2;
-    var periode = 2 * Math.PI * Math.sqrt(a*a*a/398600.4418);
-    document.getElementById('periode').innerHTML = periode + "s" ;
 });
+
+function km_vers_UA(valeur) {
+    var UA = 149597870.7;
+    valeur /= UA;
+    return valeur;
+}
+
+function UA_vers_km(valeur) {
+    var UA = 149597870.7;
+    valeur *= UA;
+    return valeur;
+}
+
+for (var i=0, c=altitude_unit.length ; i < c ; i++){
+    altitude_unit[i].addEventListener('click', function(){
+        var unit = this.innerHTML;
+        if(unit=='km'){
+            document.getElementById('labelperi').innerHTML = 'Périgée';
+            document.getElementById('labelapo').innerHTML = 'Apogée';
+            for(var j=0; j<c ; j++){
+                altitude_unit[j].innerHTML='UA';
+                var distance = document.getElementsByName('distance_group');
+                distance[j].value = km_vers_UA(distance[j].value);
+                rayon = km_vers_UA(rayon);
+            }
+        }
+        else {
+            document.getElementById('labelperi').innerHTML = 'Périgée (altitude)';
+            document.getElementById('labelapo').innerHTML = 'Apogée (altitude)';
+            for(var j=0; j<c ; j++){
+                altitude_unit[j].innerHTML='km';
+                var distance = document.getElementsByName('distance_group');
+                distance[j].value = UA_vers_km(distance[j].value)
+                rayon = UA_vers_km(rayon);
+            }
+        }
+    });
+}
 
