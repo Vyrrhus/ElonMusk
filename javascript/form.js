7fb161344cd3e70f5a889caa80f1c73e8481f0a0
Vyrrhus <baptistedesoubrie@gmail.com>
2017-09-01 00:33:00 +0200

    reupload form.js from desktop repository

--- javascript/form.js
+++ javascript/form.js
@@ -1,4 +1,4 @@
-// Déclaration des variables
+// Déclaration des nodes input & select
 
 var departForm = document.getElementById('form_depart');
 var inc = departForm.elements['inclinaison'];
@@ -9,25 +9,35 @@ var e = departForm.elements['ecc'];
 var a = departForm.elements['demiaxe'];
 var w = departForm.elements['argument'];
 var v = departForm.elements['anomaly'];
+
+// Déclaration des nodes span
+var distance_unit_dep = document.getElementsByName('distance_unit_dep');
+var angle_unit = document.getElementsByName('angle_unit');
+
+// Déclaration des variables globales
 var rayon = 6371.008;
-var altitude_unit = document.getElementsByName('altitude_unit');
 
-
-
-// Déclaration des fonctions
-
+// Déclaration des functions
 function km_vers_UA(valeur) {
     var UA = 149597870.7;
     valeur /= UA;
     return valeur;
 }
-
 function UA_vers_km(valeur) {
     var UA = 149597870.7;
     valeur *= UA;
     return valeur;
 }
-
+function deg_vers_rad(valeur){
+    var Pi = Math.PI;
+    valeur = valeur / 180 * Pi;
+    return valeur;
+}
+function rad_vers_deg(valeur) {
+    var Pi = Math.PI;
+    valeur = valeur / Pi * 180;
+    return valeur;
+}
 function warning(elementDeReference, bool) {
     var parent = elementDeReference.parentNode;
     parent = parent.parentNode;
@@ -44,10 +54,7 @@ function warning(elementDeReference, bool) {
     }
 }
 
-
-
-
-// Evenements
+// Déclaration des events
 
 inc.addEventListener('keyup', function() {
     var valeur = inc.value;
@@ -135,17 +142,14 @@ hPeri.addEventListener('keyup', function parametre() {
             e.value = (Apo - valeur) / (Apo + valeur + 2 * rayon);
             a.value = (Apo + valeur) / 2 + rayon;
         }
-        else {
+        else if (!isNaN(valeur)) {
             hApo.value = valeur;
             parametre();
         }
     }
 });
 
-
-
-
-hApo.addEventListener('keyup', function() {
+hApo.addEventListener('keyup', function parametre() {
     var valeur = hApo.value;
     var condition = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
     var longueur = valeur.length;
@@ -166,32 +170,21 @@ hApo.addEventListener('keyup', function() {
     }
     else {
         warning(hApo, false);
+        var Peri = parseFloat(hPeri.value, 10);
+        if (valeur >= Peri) {
+            e.value = (valeur - Peri) / (valeur + Peri + 2 * rayon);
+            a.value = (valeur + Peri) / 2 + rayon;
+        }
+        else if (!isNaN(valeur)) {
+            hPeri.value=valeur;
+            parametre();
+        }
+        else {
+            hPeri.value=0;
+            parametre();
+        }
     }
 });
-
-
-
-hApo.addEventListener('keypress', function parametre() {
-    var Apo = parseInt(hApo.value, 10) + rayon;
-    var Peri = parseInt(hPeri.value, 10) + rayon;
-    if (Peri <= Apo) {
-    departForm.elements['ecc'].value = (Apo - Peri) / (Apo + Peri) ;
-    departForm.elements['demiaxe'].value = (Apo + Peri) / 2 ;
-    }
-    else {
-        hPeri.value=hApo.value;
-        parametre()
-    }
-});
-
-
-
-
-
-
-
-
-
 
 w.addEventListener('keyup', function() {
     var valeur = w.value;
@@ -233,7 +226,7 @@ v.addEventListener('keyup', function() {
         }
     }
     valeur = parseFloat(valeur);
-    if (error > 0 || virgule > 1) {
+    if (error > 0 || virgule > 1 || valeur > 360) {
         warning(v, true);
     }
     else {
@@ -241,33 +234,49 @@ v.addEventListener('keyup', function() {
     }
 });
 
-
-
-
-
-for (var i=0, c=altitude_unit.length ; i < c ; i++){
-    altitude_unit[i].addEventListener('click', function(){
+for (let k=0, c=distance_unit_dep.length ; k < c ; k++) {
+    distance_unit_dep[k].addEventListener('click', function() {
         var unit = this.innerHTML;
-        if(unit=='km'){
-            document.getElementById('labelperi').innerHTML = 'Périgée';
-            document.getElementById('labelapo').innerHTML = 'Apogée';
-            for(var j=0; j<c ; j++){
-                altitude_unit[j].innerHTML='UA';
-                var distance = document.getElementsByName('distance_group');
-                distance[j].value = km_vers_UA(distance[j].value);
-                rayon = km_vers_UA(rayon);
-            }
+        if (unit == 'km') {
+            rayon = km_vers_UA(rayon);
+            distance_unit_dep.forEach(function (element){
+                element.innerHTML='UA';
+                var input = element.previousElementSibling;
+                if (input.value != ''){
+                    input.value = km_vers_UA(input.value);
+                }
+                element.setAttribute('data-content','Distance au foyer');
+            });
         }
         else {
-            document.getElementById('labelperi').innerHTML = 'Périgée (altitude)';
-            document.getElementById('labelapo').innerHTML = 'Apogée (altitude)';
-            for(var j=0; j<c ; j++){
-                altitude_unit[j].innerHTML='km';
-                var distance = document.getElementsByName('distance_group');
-                distance[j].value = UA_vers_km(distance[j].value)
-                rayon = UA_vers_km(rayon);
-            }
+            rayon = UA_vers_km(rayon);
+            distance_unit_dep.forEach(function (element) {
+                element.innerHTML='km';
+                var input = element.previousElementSibling;
+                if (input.value != ''){
+                input.value = UA_vers_km(input.value);
+                }
+                element.setAttribute('data-content','Altitude');
+            });
         }
     });
 }
+
+for (let k=0, c=angle_unit.length ; k < c ; k++) {
+    angle_unit[k].addEventListener('click', function() {
+        var unit = this.innerHTML;
+        if (unit == '°') {
+            this.innerHTML='rad';
+            var input = this.previousElementSibling;
+            input.value = deg_vers_rad(input.value);
+        }
+        else {
+            this.innerHTML='°';
+            var input = this.previousElementSibling;
+            input.value = rad_vers_deg(input.value);
+        }
+    });
+}
+
+// Déclaration du select originBody
 
